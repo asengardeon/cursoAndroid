3181a88967f78486201ad49aaeb600d35bf2ad33
asengardeon <asengardeons@hotmail.com>
2018-04-25 13:42:42 -0300

    App Adivinha - first commit

---
+++ Adivinha/app/src/main/java/com/cursoandroid/adivinha/MainActivity.java
@@ -0,0 +1,25 @@
+package com.cursoandroid.adivinha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button botaoJogar;
+    private TextView textoResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        botaoJogar = findViewById(R.id.botaoJogarId);
+        textoResultado = findViewById(R.id.resultadoId);
+
+        textoResultado.setText("Texto alterado");
+    }
+}
